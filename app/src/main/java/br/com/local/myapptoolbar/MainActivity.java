0b1219ad7a51c2c6be0b969c1fd46528a5c60072
lndsilva <lndsilva@hotmail.com>
2019-09-22 20:28:41 -0300

    criando toolbar menu

--- app/src/main/java/br/com/local/myapptoolbar/MainActivity.java
+++ app/src/main/java/br/com/local/myapptoolbar/MainActivity.java
@@ -1,14 +1,51 @@
 package br.com.local.myapptoolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.idToolBar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mEmail:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no e-mail",
+                        Toast.LENGTH_SHORT).show();
+            case R.id.mPerfil:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no perfil",
+                        Toast.LENGTH_SHORT).show();
+
+        }
+
+
+        return true;
     }
 }
