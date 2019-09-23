852b7e783f229e81e266bf8439281f0156f6ede8
lndsilva <lndsilva@hotmail.com>
2019-09-22 21:09:51 -0300

    criando toolbar menu icones

--- app/src/main/java/br/com/local/myapptoolbar/MainActivity.java
+++ app/src/main/java/br/com/local/myapptoolbar/MainActivity.java
@@ -18,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.idToolBar);
-
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_computer);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 
     @Override
